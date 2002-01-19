57a027e268221505f882707a5d8c2dc3fe677883
injektilo <injektilo>
2002-01-19 07:29:36 +0000

    Forgot to add the two new files to the deploy script.

--- web/deploy.cmd
+++ web/deploy.cmd
@@ -1,6 +1,6 @@
 @echo off
 if %1x == x goto usage
-scp style.css index.html screenshots.html cvs.html %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs
+scp style.css index.html screenshots.html download.html involved.html cvs.html %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs
 goto end
 :usage
 echo usage: deploy username
