6891fbeae6baf0b2267c93b5cb0c7fc6c8b69503
injektilo <injektilo>
2002-01-19 23:28:12 +0000

    Replaced batch files with NAnt targets to build devel package.

--- zip-release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ..
-if not exist ndoc.zip goto zip
-echo ndoc.zip exists. rename it or delete it.
-goto end
-:zip
-"C:\Program Files\WinZip\WZZIP.EXE" ndoc.zip ndoc\src\Gui\bin\Release\* ndoc\src\Gui\NDocGui.exe.manifest ndoc\src\Console\bin\Release\NDocConsole.*
-:end
-cd ndoc
--- zip-src.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ..
-if not exist ndoc-src.zip goto zip
-echo ndoc-src.zip exists. rename it or delete it.
-goto end
-:zip
-"C:\Program Files\WinZip\WZZIP.EXE" -P -x@ndoc\x-files.txt ndoc-src.zip @ndoc\files.txt
-:end
-cd ndoc
