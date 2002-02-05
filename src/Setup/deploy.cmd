7d2e3cc6f0b492923dac9232250e2ee7cdaf6c94
jcman <jcman>
2002-02-05 00:58:25 +0000

    Added deployment script.

---
+++ src/Setup/deploy.cmd
@@ -0,0 +1,18 @@
+@echo off
+cd Release
+if %1x == x goto usage
+if %2x == -allx goto deployall
+
+scp NDocSetup.msi %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs/setup
+goto end
+
+:deployall
+scp InstMsiA.Exe InstMsiW.Exe NDocSetup.msi Setup.Exe %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs/setup
+goto end
+
+:usage
+echo usage: deploy username [-all]
+
+:end
+cd ..
+
