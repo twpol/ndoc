67ed4f35f237348bc0bb5d77e25001631512310f
injektilo <injektilo>
2002-07-27 06:42:49 +0000

    Don't forgot to deploy the new resources page.

--- web/deploy.cmd
+++ web/deploy.cmd
@@ -1,6 +1,6 @@
 @echo off
 if %1x == x goto usage
-scp style.css index.html screenshots.html download.html involved.html cvs.html users.html %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs
+scp style.css index.html screenshots.html download.html involved.html cvs.html users.html resources.html %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs
 goto end
 :usage
 echo usage: deploy username
