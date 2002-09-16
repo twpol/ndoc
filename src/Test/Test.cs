64a84854b28465fd84b9ec2af5d51202f059ce57
mhjp <mhjp>
2002-09-16 21:11:50 +0000

    Added test cases for testing the new modifier

--- src/Test/Test.cs
+++ src/Test/Test.cs
@@ -1416,6 +1416,309 @@ namespace NDoc.Test
 	}
 }
 
+namespace NDoc.Test.NewStuff
+{
+	/// <summary>
+	/// Base class used to test the new modifier. See <see cref="NewDerived"/>
+	/// for details. Members in this class are named to demonstrate syntax
+	/// in the derived class.
+	/// </summary>
+	public class NewBase
+	{
+		/// <summary></summary>
+		public class NewClass
+		{
+		}
+		/// <summary></summary>
+		public int this[int n]
+		{
+			get
+			{
+				return 0;
+			}
+			set
+			{
+			}
+		}
+
+		/// <summary></summary>
+		public void NewPropertySNOKOM(int n)
+		{
+		}
+		/// <summary></summary>
+		public void NewVirtualPropertySNOKOM(int n)
+		{
+		}
+		/// <summary></summary>
+		public int NewMethodSNOKOM;
+		/// <summary></summary>
+		public int NewVirtualMethodSNOKOM;
+		/// <summary></summary>
+		public int NewFieldSNOKOM
+		{
+			get { return 0; }
+			set { }
+		}
+
+		/// <summary></summary>
+		public const int NewConst = 500;
+		/// <summary></summary>
+		public int NewField;
+
+		/// <summary></summary>
+		public int NewProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public virtual int OverrideProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public virtual int NewVirtualProperty
+		{
+			get { return 0; }
+			set {}
+		}
+
+		/// <summary></summary>
+		public void NewMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public virtual void OverrideMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public virtual void NewVirtualMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public void NewMethodWithOverload(int n)
+		{
+		}
+		/// <summary></summary>
+		public virtual void NewMethodWithOverload(double d)
+		{
+		}
+		/// <summary></summary>
+		public virtual void NewMethodWithOverload(long l)
+		{
+		}
+		/// <summary>
+		/// public void NewMethodWithOverload(short h)
+		/// </summary>
+		/// <param name="h"></param>
+		public void NewMethodWithOverload(short h)
+		{
+		}
+
+		/// <summary></summary>
+		public void NewStaticMethod(int n)
+		{
+		}
+	}
+	/// <summary>
+	/// This class provides new implementations for the base class members.
+	/// </summary>
+	public class NewDerived : NewBase
+	{
+		/// <summary></summary>
+		new public class NewClass
+		{
+		}
+		/// <summary></summary>
+		new public int this[int n]
+		{
+			get
+			{
+				return 0;
+			}
+			set
+			{
+			}
+		}
+		/// <summary></summary>
+		public int this[string n]
+		{
+			get
+			{
+				return 0;
+			}
+			set
+			{
+			}
+		}
+
+		/// <summary></summary>
+		public const int Const = 500;
+		/// <summary></summary>
+		public new const int NewConst = 500;
+		/// <summary></summary>
+		public int Field;
+		/// <summary></summary>
+		public new int NewField;
+		/// <summary>
+		/// "Same Name Other Kind Of Member"
+		/// </summary>
+		public new int NewFieldSNOKOM;
+
+		/// <summary></summary>
+		public int Property
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public new int NewProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public virtual int VirtualProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public override int OverrideProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary></summary>
+		public new virtual int NewVirtualProperty
+		{
+			get { return 0; }
+			set {}
+		}
+		/// <summary>
+		/// "Same Name Other Kind Of Member"
+		/// </summary>
+		public new int NewPropertySNOKOM
+		{
+			get { return 0; }
+			set { }
+		}
+		/// <summary>
+		/// "Same Name Other Kind Of Member"
+		/// </summary>
+		public new virtual int NewVirtualPropertySNOKOM
+		{
+			get { return 0; }
+			set { }
+		}
+		/// <summary></summary>
+		public void Method(int n)
+		{
+		}
+		/// <summary></summary>
+		public new void NewMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public virtual void VirtualMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public override void OverrideMethod(int n)
+		{
+		}
+		/// <summary></summary>
+		public new virtual void NewVirtualMethod(int n)
+		{
+		}
+		/// <summary>
+		/// "Same Name Other Kind Of Member"
+		/// </summary>
+		public new void NewMethodSNOKOM(int n)
+		{
+		}
+		/// <summary>
+		/// "Same Name Other Kind Of Member"
+		/// </summary>
+		public new virtual void NewVirtualMethodSNOKOM(int n)
+		{
+		}
+		/// <summary>
+		/// public void NewMethodWithOverload(string s)
+		/// </summary>
+		/// <param name="s"></param>
+		public void NewMethodWithOverload(string s)
+		{
+		}
+		/// <summary>
+		/// new public void NewMethodWithOverload(int n)
+		/// </summary>
+		/// <param name="n"></param>
+		public new void NewMethodWithOverload(int n)
+		{
+		}
+		/// <summary>
+		/// public virtual void NewMethodWithOverload(object o)
+		/// </summary>
+		/// <param name="o"></param>
+		public virtual void NewMethodWithOverload(object o)
+		{
+		}
+		/// <summary>
+		/// public override void NewMethodWithOverload(double d)
+		/// </summary>
+		/// <param name="d"></param>
+		public override void NewMethodWithOverload(double d)
+		{
+		}
+		/// <summary>
+		/// new public virtual void NewMethodWithOverload(long l)
+		/// </summary>
+		/// <param name="l"></param>
+		public new virtual void NewMethodWithOverload(long l)
+		{
+		}
+
+		/// <summary></summary>
+		public new static void NewStaticMethod(int n)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Do you see F?
+	/// </summary>
+	public class Base
+	{
+		/// <summary></summary>
+		public static void F() {}
+	}
+	/// <summary>
+	/// Now F is gone (private)!
+	/// </summary>
+	public class Derived: Base
+	{
+		new private static void F() {}   // Hides Base.F in Derived only
+	}
+	/// <summary>
+	/// Where is F?
+	/// </summary>
+	public class MoreDerived: Derived
+	{
+	}
+	/// <summary>
+	/// Uses MoreDerived.F (actually Base.F)
+	/// </summary>
+	public class SomeClass
+	{
+		/// <summary>
+		/// Works, so MoreDerived.F is there, but where?
+		/// </summary>
+		static void G() { MoreDerived.F(); }         // Invokes Base.F
+	}
+}
+
 namespace NDoc.Test.InternalStuff
 {
 	internal class InternalClass
