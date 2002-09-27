c05414834c0f477f1efd83b5d8649a85c1058c13
clubstew <clubstew>
2002-09-27 03:17:27 +0000

    Was empty - added information.

--- src/Addins/NDocBuild/AssemblyInfo.cs
+++ src/Addins/NDocBuild/AssemblyInfo.cs
@@ -6,11 +6,12 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NDoc Visual Studio .NET Add-in")]
+[assembly: AssemblyDescription(
+"Add-in for Visual Studio .NET to generate documentation from source files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("ndoc.sourceforge.net")]
+[assembly: AssemblyProduct("NDoc")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
@@ -26,7 +27,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -47,5 +48,5 @@ using System.Runtime.CompilerServices;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile(@"..\..\..\ndoc.snk")]
+[assembly: AssemblyKeyFile(@"..\..\..\..\ndoc.snk")]
 [assembly: AssemblyKeyName("")]
