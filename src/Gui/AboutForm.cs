6944e133bc9dc8371e6e4b4cd8b05f666a695192
jcman <jcman>
2002-09-30 01:33:33 +0000

    Updated credits.

--- src/Gui/AboutForm.cs
+++ src/Gui/AboutForm.cs
@@ -33,14 +33,14 @@ namespace NDoc.Gui
 		#region Fields
 		private System.Windows.Forms.Button closeButton;
 		private System.Windows.Forms.RichTextBox richTextBox;
-		private System.Windows.Forms.LinkLabel mailLinkLabel;
-		private System.Windows.Forms.LinkLabel webLinkLabel;
 		private NDoc.Gui.HeaderGroupBox versionHeaderGroupBox;
 		private System.Windows.Forms.ColumnHeader assemblyColumnHeader;
 		private System.Windows.Forms.ListView assembliesListView;
 		private System.Windows.Forms.ColumnHeader versionColumnHeader;
-		private NDoc.Gui.HeaderGroupBox contactInfoHeaderGroupBox;
 		private System.Windows.Forms.ColumnHeader dateColumnHeader;
+		private System.Windows.Forms.LinkLabel projectHomePageLinkLabel;
+		private System.Windows.Forms.LinkLabel adminsLinkLabel;
+		private System.Windows.Forms.Label developersLabel;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -64,18 +64,11 @@ namespace NDoc.Gui
 			Stream stream = assembly.GetManifestResourceStream("NDoc.Gui.About.rtf");
 			richTextBox.LoadFile(stream, RichTextBoxStreamType.RichText);
 
-			// Set up email links
-			mailLinkLabel.Links.Add(13, 13, "http://sourceforge.net/sendmessage.php?touser=87620");
-			mailLinkLabel.Links.Add(28, 10, "http://sourceforge.net/sendmessage.php?touser=97544");
-			mailLinkLabel.Links.Add(40, 18, "http://sourceforge.net/sendmessage.php?touser=235364");
-			mailLinkLabel.Links.Add(60, 10, "http://sourceforge.net/sendmessage.php?touser=129858");
-			mailLinkLabel.Links.Add(75, 14, "http://sourceforge.net/sendmessage.php?touser=399623");
-
 			// Set up web links
-			webLinkLabel.Links.Add(12, 12, "http://sourceforge.net/projects/ndoc");
-			webLinkLabel.Links.Add(26, 13, "http://sourceforge.net/tracker/?func=add&group_id=36057&atid=416078");
-			webLinkLabel.Links.Add(41, 11, "http://sourceforge.net/mail/?group_id=36057");
-			webLinkLabel.Links.Add(54, 10, "http://www.opensource.org/");
+			projectHomePageLinkLabel.Links.Add(19, 28, "http://ndoc.sourceforge.net");
+			adminsLinkLabel.Links.Add(24, 13, "http://sourceforge.net/sendmessage.php?touser=87620");
+			adminsLinkLabel.Links.Add(39, 18, "http://sourceforge.net/sendmessage.php?touser=235364");
+			adminsLinkLabel.Links.Add(59, 10, "http://sourceforge.net/sendmessage.php?touser=97544");
 
 			// Fill in loaded modules / version number info list view.
 			try 
@@ -147,33 +140,18 @@ namespace NDoc.Gui
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mailLinkLabel = new System.Windows.Forms.LinkLabel();
 			this.assembliesListView = new System.Windows.Forms.ListView();
 			this.assemblyColumnHeader = new System.Windows.Forms.ColumnHeader();
 			this.versionColumnHeader = new System.Windows.Forms.ColumnHeader();
 			this.dateColumnHeader = new System.Windows.Forms.ColumnHeader();
-			this.contactInfoHeaderGroupBox = new NDoc.Gui.HeaderGroupBox();
-			this.webLinkLabel = new System.Windows.Forms.LinkLabel();
 			this.closeButton = new System.Windows.Forms.Button();
 			this.richTextBox = new System.Windows.Forms.RichTextBox();
 			this.versionHeaderGroupBox = new NDoc.Gui.HeaderGroupBox();
-			this.contactInfoHeaderGroupBox.SuspendLayout();
+			this.projectHomePageLinkLabel = new System.Windows.Forms.LinkLabel();
+			this.adminsLinkLabel = new System.Windows.Forms.LinkLabel();
+			this.developersLabel = new System.Windows.Forms.Label();
 			this.versionHeaderGroupBox.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// mailLinkLabel
-			// 
-			this.mailLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right);
-			this.mailLinkLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.mailLinkLabel.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
-			this.mailLinkLabel.Location = new System.Drawing.Point(8, 16);
-			this.mailLinkLabel.Name = "mailLinkLabel";
-			this.mailLinkLabel.Size = new System.Drawing.Size(504, 16);
-			this.mailLinkLabel.TabIndex = 0;
-			this.mailLinkLabel.Text = "Developers:  Jason Diamond, Kral Ferch, Jean-Claude Manoli, Keith Hill and Pascal" +
-				" Bourque";
-			this.mailLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.mailLinkLabel_LinkClicked);
 			// 
 			// assembliesListView
 			// 
@@ -187,7 +165,7 @@ namespace NDoc.Gui
 			this.assembliesListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
 			this.assembliesListView.Location = new System.Drawing.Point(8, 16);
 			this.assembliesListView.Name = "assembliesListView";
-			this.assembliesListView.Size = new System.Drawing.Size(504, 114);
+			this.assembliesListView.Size = new System.Drawing.Size(510, 114);
 			this.assembliesListView.TabIndex = 0;
 			this.assembliesListView.View = System.Windows.Forms.View.Details;
 			// 
@@ -206,77 +184,94 @@ namespace NDoc.Gui
 			this.dateColumnHeader.Text = "Date";
 			this.dateColumnHeader.Width = 124;
 			// 
-			// contactInfoHeaderGroupBox
-			// 
-			this.contactInfoHeaderGroupBox.Controls.AddRange(new System.Windows.Forms.Control[] {
-																									this.webLinkLabel,
-																									this.mailLinkLabel});
-			this.contactInfoHeaderGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.contactInfoHeaderGroupBox.Location = new System.Drawing.Point(8, 152);
-			this.contactInfoHeaderGroupBox.Name = "contactInfoHeaderGroupBox";
-			this.contactInfoHeaderGroupBox.Padding = 0;
-			this.contactInfoHeaderGroupBox.Size = new System.Drawing.Size(520, 56);
-			this.contactInfoHeaderGroupBox.TabIndex = 5;
-			this.contactInfoHeaderGroupBox.TabStop = false;
-			this.contactInfoHeaderGroupBox.Text = "Contact Information:";
-			// 
-			// webLinkLabel
-			// 
-			this.webLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right);
-			this.webLinkLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.webLinkLabel.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
-			this.webLinkLabel.Location = new System.Drawing.Point(8, 32);
-			this.webLinkLabel.Name = "webLinkLabel";
-			this.webLinkLabel.Size = new System.Drawing.Size(504, 16);
-			this.webLinkLabel.TabIndex = 1;
-			this.webLinkLabel.Text = "Web Links:  NDoc Project, Submit Defect, List Server, OpenSource";
-			this.webLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.webLinkLabel_LinkClicked);
-			// 
 			// closeButton
 			// 
 			this.closeButton.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right);
 			this.closeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.closeButton.Location = new System.Drawing.Point(452, 372);
+			this.closeButton.Location = new System.Drawing.Point(458, 394);
 			this.closeButton.Name = "closeButton";
 			this.closeButton.TabIndex = 0;
 			this.closeButton.Text = "&Close";
 			// 
 			// richTextBox
 			// 
+			this.richTextBox.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+				| System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right);
 			this.richTextBox.BackColor = System.Drawing.SystemColors.Control;
 			this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.richTextBox.Location = new System.Drawing.Point(16, 8);
 			this.richTextBox.Name = "richTextBox";
 			this.richTextBox.ReadOnly = true;
-			this.richTextBox.Size = new System.Drawing.Size(512, 136);
+			this.richTextBox.Size = new System.Drawing.Size(518, 136);
 			this.richTextBox.TabIndex = 4;
 			this.richTextBox.Text = "";
 			// 
 			// versionHeaderGroupBox
 			// 
-			this.versionHeaderGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-				| System.Windows.Forms.AnchorStyles.Left);
+			this.versionHeaderGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right);
 			this.versionHeaderGroupBox.Controls.AddRange(new System.Windows.Forms.Control[] {
 																								this.assembliesListView});
 			this.versionHeaderGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.versionHeaderGroupBox.Location = new System.Drawing.Point(8, 218);
+			this.versionHeaderGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+			this.versionHeaderGroupBox.Location = new System.Drawing.Point(8, 240);
 			this.versionHeaderGroupBox.Name = "versionHeaderGroupBox";
 			this.versionHeaderGroupBox.Padding = 0;
-			this.versionHeaderGroupBox.Size = new System.Drawing.Size(520, 144);
+			this.versionHeaderGroupBox.Size = new System.Drawing.Size(526, 144);
 			this.versionHeaderGroupBox.TabIndex = 1;
 			this.versionHeaderGroupBox.TabStop = false;
 			this.versionHeaderGroupBox.Text = "Version Information";
+			// 
+			// projectHomePageLinkLabel
+			// 
+			this.projectHomePageLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right);
+			this.projectHomePageLinkLabel.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.projectHomePageLinkLabel.Location = new System.Drawing.Point(16, 152);
+			this.projectHomePageLinkLabel.Name = "projectHomePageLinkLabel";
+			this.projectHomePageLinkLabel.Size = new System.Drawing.Size(520, 16);
+			this.projectHomePageLinkLabel.TabIndex = 5;
+			this.projectHomePageLinkLabel.TabStop = true;
+			this.projectHomePageLinkLabel.Text = "Project home page: http://ndoc.sourceforge.net";
+			this.projectHomePageLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.projectHomePageLinkLabel_LinkClicked);
+			// 
+			// adminsLinkLabel
+			// 
+			this.adminsLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right);
+			this.adminsLinkLabel.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.adminsLinkLabel.Location = new System.Drawing.Point(16, 176);
+			this.adminsLinkLabel.Name = "adminsLinkLabel";
+			this.adminsLinkLabel.Size = new System.Drawing.Size(520, 16);
+			this.adminsLinkLabel.TabIndex = 6;
+			this.adminsLinkLabel.TabStop = true;
+			this.adminsLinkLabel.Text = "Project administrators: Jason Diamond, Jean-Claude Manoli, Kral Ferch";
+			this.adminsLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.adminsLinkLabel_LinkClicked);
+			// 
+			// developersLabel
+			// 
+			this.developersLabel.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right);
+			this.developersLabel.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.developersLabel.Location = new System.Drawing.Point(16, 200);
+			this.developersLabel.Name = "developersLabel";
+			this.developersLabel.Size = new System.Drawing.Size(512, 32);
+			this.developersLabel.TabIndex = 7;
+			this.developersLabel.Text = "Contributors: Heath Stewart, Jerome Mathieu, Keith Hill, Michael Poettgen, Pascal" +
+				" Bourque, Ryan Seghers, Steve Van Esch, Thong (Tum) Nguyen";
 			// 
 			// AboutForm
 			// 
 			this.AcceptButton = this.closeButton;
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
 			this.CancelButton = this.closeButton;
-			this.ClientSize = new System.Drawing.Size(540, 410);
+			this.ClientSize = new System.Drawing.Size(546, 432);
 			this.Controls.AddRange(new System.Windows.Forms.Control[] {
-																		  this.contactInfoHeaderGroupBox,
+																		  this.developersLabel,
+																		  this.adminsLinkLabel,
+																		  this.projectHomePageLinkLabel,
 																		  this.richTextBox,
 																		  this.versionHeaderGroupBox,
 																		  this.closeButton});
@@ -286,27 +281,26 @@ namespace NDoc.Gui
 			this.Name = "AboutForm";
 			this.ShowInTaskbar = false;
 			this.Text = "About NDoc";
-			this.contactInfoHeaderGroupBox.ResumeLayout(false);
 			this.versionHeaderGroupBox.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 		#endregion
 
-		#region Event Handlers
-		private void mailLinkLabel_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+		#region event handlers
+		private void projectHomePageLinkLabel_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
 		{
-			mailLinkLabel.Links[mailLinkLabel.Links.IndexOf(e.Link)].Visited = true;
+			projectHomePageLinkLabel.Links[projectHomePageLinkLabel.Links.IndexOf(e.Link)].Visited = true;
 			string url = e.Link.LinkData.ToString();
 			Process.Start(url);
 		}
 
-		private void webLinkLabel_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+		private void adminsLinkLabel_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
 		{
-			webLinkLabel.Links[webLinkLabel.Links.IndexOf(e.Link)].Visited = true;
+			adminsLinkLabel.Links[adminsLinkLabel.Links.IndexOf(e.Link)].Visited = true;
 			string url = e.Link.LinkData.ToString();
 			Process.Start(url);
 		}
-		#endregion // Event Handlers
+		#endregion
 	}
 }
