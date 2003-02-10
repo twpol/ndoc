bc186680c20be22e201b179734cfbf52a6fdf4e5
jcman <jcman>
2003-02-10 17:01:34 +0000

    Fixed case in documenter name.

--- src/Documenter/Latex/LatexDocumenterConfig.cs
+++ src/Documenter/Latex/LatexDocumenterConfig.cs
@@ -32,7 +32,7 @@ namespace NDoc.Documenter.Latex
 	public class LatexDocumenterConfig
 		: BaseDocumenterConfig
 	{
-		public LatexDocumenterConfig() : base("Latex")
+		public LatexDocumenterConfig() : base("LateX")
 		{			
 			OutputDirectory = "Tex";
 			TexFileBaseName = "Documentation";
