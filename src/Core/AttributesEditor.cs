f17241977dc6b28cb85e8f7487457fb80124ff2b
drieseng <drieseng>
2003-07-06 17:53:40 +0000

    removed unnecessary using directives

--- src/Core/AttributesEditor.cs
+++ src/Core/AttributesEditor.cs
@@ -1,13 +1,7 @@
 using System;
 using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Design;
 using System.Windows.Forms;
-using System.Windows.Forms.ComponentModel;
-using System.Windows.Forms.Design;
 
 namespace NDoc.Core
 {
