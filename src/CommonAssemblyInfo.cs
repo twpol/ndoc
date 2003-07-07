965bc9632697f90a8f8229c77b50a4514fad73e7
drieseng <drieseng>
2003-07-07 07:23:32 +0000

    added CommonAssemblyInfo.cs for assembly-level attributes that all NDoc assemblies should share

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ndoc.sourceforge.net")]
+[assembly: AssemblyProduct("NDoc")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]			
+[assembly: AssemblyVersion("1.2.*")]
