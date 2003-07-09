360b372092a62bd07f7caf85eb5504b3559e2845
mhjp <mhjp>
2003-07-09 01:14:16 +0000

    Modified AssemblyInfo.cs to display constant AssemblyVersion. Removed reference to CommonAssemblyInfo.cs (not a retail module). This enables comparison to older NDoc outputs when testing bugfixes.

--- src/Test/AssemblyInfo.cs
+++ src/Test/AssemblyInfo.cs
@@ -1,7 +1,21 @@
+using System;
 using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+
+// Common
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ndoc.sourceforge.net")]
+[assembly: AssemblyProduct("NDoc")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]			
 
 [assembly: AssemblyTitle("NDoc Test")]
 [assembly: AssemblyDescription("Test class library for the NDoc code documentation generator.")]
+
+// When kept constant, results of tests can be compared (windiffed) against older output.
+[assembly: AssemblyVersion("1.2.1285.524")]
 
 #if !DEBUG
 [assembly: AssemblyDelaySign(false)]
