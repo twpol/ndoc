d908bb6997b99621fa0485fe6ed32c29caf97127
drieseng <drieseng>
2004-03-13 17:06:41 +0000

    include CommonAssemblyInfo.cs

--- src/VisualStudio/AssemblyInfo.cs
+++ src/VisualStudio/AssemblyInfo.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Reflection;
 
+[assembly: CLSCompliantAttribute(true)]
 [assembly: AssemblyTitle("NDoc VS.NET Solution Parser")]
 [assembly: AssemblyDescription("Visual Studio solutionparser for the NDoc code documentation generator.")]
 
