67ffe288cd31b0c7c2815707a05d4502e5844212
dkackman <dkackman>
2004-03-31 02:06:44 +0000

    Added some using statements and now dispose the StringFormat object

--- src/Gui/HeaderGroupBox.cs
+++ src/Gui/HeaderGroupBox.cs
@@ -45,55 +45,61 @@ namespace NDoc.Gui
 		/// <param name="e"></param>
 		protected override void OnPaint(PaintEventArgs e)
 		{
-			StringFormat format = new StringFormat();
-			format.Trimming = StringTrimming.Character;
-			format.Alignment = StringAlignment.Near;
-			if (this.RightToLeft == RightToLeft.Yes)
-			{
-				format.FormatFlags = format.FormatFlags | StringFormatFlags.DirectionRightToLeft;
-			}
+			using ( StringFormat format = new StringFormat() )
+			{			
+				format.Trimming = StringTrimming.Character;
+				format.Alignment = StringAlignment.Near;
 
-			Rectangle textRectangle = Rectangle.Inflate( ClientRectangle, -padding, 0 );
+				if (this.RightToLeft == RightToLeft.Yes)
+				{
+					format.FormatFlags = format.FormatFlags | StringFormatFlags.DirectionRightToLeft;
+				}
 
-			SizeF stringSize = e.Graphics.MeasureString(Text, Font, textRectangle.Size, format);
+				Rectangle textRectangle = Rectangle.Inflate( ClientRectangle, -padding, 0 );
 
-			if (Enabled)
-			{
-				Brush br = new SolidBrush(ForeColor);
-				e.Graphics.DrawString(Text, Font, br, textRectangle, format);
-				br.Dispose();
-			}
-			else
-			{
-				ControlPaint.DrawStringDisabled(e.Graphics, Text, Font, BackColor, textRectangle, format);
-			}
+				SizeF stringSize = e.Graphics.MeasureString(Text, Font, textRectangle.Size, format);
 
-			Pen forePen = new Pen(ControlPaint.LightLight( BackColor ), SystemInformation.BorderSize.Height);
-			Pen forePenDark = new Pen(ControlPaint.Dark( BackColor ), SystemInformation.BorderSize.Height);
-			Point lineLeft = new Point(textRectangle.Left, textRectangle.Top + (int)(Font.Height / 2f));
-			Point lineRight = new Point(textRectangle.Right, textRectangle.Top + (int)(Font.Height / 2f));
-			if (this.RightToLeft != RightToLeft.Yes)
-			{
-				lineLeft.X += (int)stringSize.Width;
-			}
-			else
-			{
-				lineRight.X -= (int)stringSize.Width;
-			}
+				if (Enabled)
+				{
+					using( Brush br = new SolidBrush(ForeColor) )
+						e.Graphics.DrawString(Text, Font, br, textRectangle, format);
+				}
+				else
+				{
+					ControlPaint.DrawStringDisabled(e.Graphics, Text, Font, BackColor, textRectangle, format);
+				}
 
-			if (FlatStyle == FlatStyle.Flat)
-			{
-				e.Graphics.DrawLine(forePenDark, lineLeft, lineRight);                        	
+
+				Point lineLeft = new Point(textRectangle.Left, textRectangle.Top + (int)(Font.Height / 2f));
+				Point lineRight = new Point(textRectangle.Right, textRectangle.Top + (int)(Font.Height / 2f));
+
+				if (this.RightToLeft != RightToLeft.Yes)
+				{
+					lineLeft.X += (int)stringSize.Width;
+				}
+				else
+				{
+					lineRight.X -= (int)stringSize.Width;
+				}
+
+				using ( Pen forePenDark = new Pen(ControlPaint.Dark( BackColor ), SystemInformation.BorderSize.Height) )
+				{
+					if (FlatStyle == FlatStyle.Flat)
+					{
+						e.Graphics.DrawLine(forePenDark, lineLeft, lineRight);                        	
+					}
+					else
+					{
+						using ( Pen forePen = new Pen(ControlPaint.LightLight( BackColor ), SystemInformation.BorderSize.Height) )
+						{
+							e.Graphics.DrawLine(forePenDark, lineLeft, lineRight);                        	
+							lineLeft.Offset(0, (int)Math.Ceiling((float)SystemInformation.BorderSize.Height / 2f));
+							lineRight.Offset(0, (int)Math.Ceiling((float)SystemInformation.BorderSize.Height / 2f));
+							e.Graphics.DrawLine(forePen, lineLeft, lineRight);
+						}
+					}
+				}
 			}
-			else
-			{
-				e.Graphics.DrawLine(forePenDark, lineLeft, lineRight);                        	
-				lineLeft.Offset(0, (int)Math.Ceiling((float)SystemInformation.BorderSize.Height / 2f));
-				lineRight.Offset(0, (int)Math.Ceiling((float)SystemInformation.BorderSize.Height / 2f));
-				e.Graphics.DrawLine(forePen, lineLeft, lineRight);
-			}
-			forePen.Dispose();
-			forePenDark.Dispose();
 		}
 	}
 }
