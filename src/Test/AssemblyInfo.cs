c55148c196f1ce9a17cd18c90652cc72d87a39e8
kdowns <kdowns>
2004-11-07 05:28:04 +0000

    Added FileVersion attribute

--- src/Test/AssemblyInfo.cs
+++ src/Test/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 [assembly: CLSCompliantAttribute(false)]
 [assembly: AssemblyTitle("NDoc Test")]
 [assembly: AssemblyDescription("Test class library for the NDoc code documentation generator.")]
+[assembly: AssemblyFileVersion("99.88.77.66")]
 
 #if (OFFICIAL_RELEASE)
 [assembly: AssemblyDelaySign(false)]
