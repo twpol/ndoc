f6135bd21c2af1c42af6c6cfb19b7b2fc61fb26f
kdowns <kdowns>
2004-11-16 17:48:37 +0000

    added license notice. updated documentation comments.

--- src/Core/Workspace.cs
+++ src/Core/Workspace.cs
@@ -1,3 +1,17 @@
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 using System;
 using System.IO;
 using System.Diagnostics;
@@ -44,7 +58,7 @@ namespace NDoc.Core
 		private string buildDir;
 
 		/// <summary>
-		/// Contructs a new instance of the Workspace class
+		/// Creates a new instance of the <see cref="Workspace"/> class.
 		/// </summary>
 		/// <param name="root">The location to create the workspace</param>
 		/// <param name="type">The type of workspace</param>
