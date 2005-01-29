2ae72de3ec6da521aff22c86336fbcee0b31804a
dkackman <dkackman>
2005-01-29 00:40:42 +0000

    Added context menu to trace window

--- src/Gui/TraceWindowControl.cs
+++ src/Gui/TraceWindowControl.cs
@@ -19,6 +19,11 @@ namespace NDoc.Gui
 		private System.Windows.Forms.Button closeButton;
 		private System.Windows.Forms.Label captionLabel;
 		private System.Windows.Forms.ToolTip toolTip1;
+		private System.Windows.Forms.ContextMenu contextMenu1;
+		private System.Windows.Forms.MenuItem clearMenuItem;
+		private System.Windows.Forms.MenuItem copyAllMenuItem;
+		private System.Windows.Forms.MenuItem copySelectionMenuItem;
+		private System.Windows.Forms.MenuItem menuItem1;
 		private System.ComponentModel.IContainer components;
 
 		/// <summary>
@@ -104,6 +109,11 @@ namespace NDoc.Gui
 			this.closeButton = new System.Windows.Forms.Button();
 			this.captionLabel = new System.Windows.Forms.Label();
 			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+			this.clearMenuItem = new System.Windows.Forms.MenuItem();
+			this.copyAllMenuItem = new System.Windows.Forms.MenuItem();
+			this.copySelectionMenuItem = new System.Windows.Forms.MenuItem();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
 			this.SuspendLayout();
 			// 
 			// richTextBox1
@@ -111,7 +121,9 @@ namespace NDoc.Gui
 			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
 				| System.Windows.Forms.AnchorStyles.Left) 
 				| System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.AutoWordSelection = true;
 			this.richTextBox1.CausesValidation = false;
+			this.richTextBox1.ContextMenu = this.contextMenu1;
 			this.richTextBox1.DetectUrls = false;
 			this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			this.richTextBox1.HideSelection = false;
@@ -122,6 +134,7 @@ namespace NDoc.Gui
 			this.richTextBox1.TabIndex = 0;
 			this.richTextBox1.Text = "";
 			this.richTextBox1.WordWrap = false;
+			this.richTextBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseDown);
 			// 
 			// closeButton
 			// 
@@ -150,6 +163,38 @@ namespace NDoc.Gui
 			this.captionLabel.Size = new System.Drawing.Size(323, 16);
 			this.captionLabel.TabIndex = 2;
 			this.captionLabel.Text = "Output";
+			// 
+			// contextMenu1
+			// 
+			this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																						 this.copyAllMenuItem,
+																						 this.copySelectionMenuItem,
+																						 this.menuItem1,
+																						 this.clearMenuItem});
+			// 
+			// clearMenuItem
+			// 
+			this.clearMenuItem.Index = 3;
+			this.clearMenuItem.Text = "Clear";
+			this.clearMenuItem.Click += new System.EventHandler(this.clearMenuItem_Click);
+			// 
+			// copyAllMenuItem
+			// 
+			this.copyAllMenuItem.Index = 0;
+			this.copyAllMenuItem.Text = "Copy All";
+			this.copyAllMenuItem.Click += new System.EventHandler(this.copyAllMenuItem_Click);
+			// 
+			// copySelectionMenuItem
+			// 
+			this.copySelectionMenuItem.Enabled = false;
+			this.copySelectionMenuItem.Index = 1;
+			this.copySelectionMenuItem.Text = "Copy Selection";
+			this.copySelectionMenuItem.Click += new System.EventHandler(this.copySelectionMenuItem_Click);
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 2;
+			this.menuItem1.Text = "-";
 			// 
 			// TraceWindowControl
 			// 
@@ -230,6 +275,31 @@ namespace NDoc.Gui
 		{
 			if ( this.Focused )
 				this.Parent.Focus();
+		}
+
+		private void copySelectionMenuItem_Click(object sender, System.EventArgs e)
+		{
+			Clipboard.SetDataObject( this.richTextBox1.SelectedText, true );
+		}
+
+		private void copyAllMenuItem_Click(object sender, System.EventArgs e)
+		{
+			Clipboard.SetDataObject( this.richTextBox1.Text, true );
+		}
+
+		private void clearMenuItem_Click(object sender, System.EventArgs e)
+		{
+			this.richTextBox1.Clear();
+		}
+
+		private void richTextBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if ( e.Button == MouseButtons.Right )
+			{
+				this.clearMenuItem.Enabled = this.richTextBox1.Text.Length > 0;
+				this.copyAllMenuItem.Enabled = this.richTextBox1.Text.Length > 0;
+				this.copySelectionMenuItem.Enabled = this.richTextBox1.SelectedText.Length > 0;		
+			}
 		}
 	}
 }
