a0ede9a16f23908257678e9784087b4a80913c27
xharzej <>
2010-07-04 22:47:17 +0000

    Removed Latex, Intellisense, LinearHtml and JavaDoc documenters

--- src/Documenter/Intellisense/xslt/StyleSheetLocation.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NDoc3.Documenter.Intellisense.xslt {
-	/// <summary>
-	/// Denotes the location of embedded stylesheet resources
-	/// </summary>
-	internal static class StyleSheetLocation {}
-}
