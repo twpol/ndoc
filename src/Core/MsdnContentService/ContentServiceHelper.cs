153634ec798f9b42b6b5a3bce6efc75b9804fd91
xharzej <>
2010-07-04 21:25:30 +0000

    Added MSDN content service, which are going to be used to get the online MSDn links to generics. It aren't used anywhere yet.

---
+++ src/Core/MsdnContentService/ContentServiceHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Services.Protocols;
+using System.Xml;
+
+namespace NDoc3.MsdnContentService {
+	public class ContentServiceHelper {
+		private const string MSDN_URL_TEMPLATE = "http://msdn.microsoft.com/{0}/library/{1}({2}).aspx";
+		private static Dictionary<string, string> cache = new Dictionary<string, string>();
+		private string locale;
+		private string version;
+
+		public ContentServiceHelper(string locale, string version) {
+			this.locale = locale;
+			this.version = version;
+		}
+
+		public string GetLink(string alias) {
+			string link = GetLinkFromCache(alias);
+			if (!String.IsNullOrEmpty(link)) return link;
+
+			getContentRequest request = new getContentRequest();
+			request.contentIdentifier = "AssetId:" + alias;
+			request.locale = locale;
+			request.version = version;
+
+			ContentServicePortTypeClient proxy = new ContentServicePortTypeClient();
+			getContentResponse response = null;
+			appId id = new appId();
+			id.value = "NDoc3";
+			try {
+				response = proxy.GetContent(id, request);
+			} catch (SoapException e) {
+				throw new Exception("Exception occurred while contacting MSDN Content Service", e);
+			}
+			string content = response.contentId;
+			return String.Format(MSDN_URL_TEMPLATE, locale, response.contentId, version);
+		}
+
+		private string GetLinkFromCache(string alias) {
+			return cache.ContainsKey(alias) ? cache[alias] : null;
+		}
+	}
+}
---
+++ test/Core.Test/MsdnContentServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using NDoc3.MsdnContentService;
+
+namespace Core.Test {
+	public class MsdnContentServiceTests {
+		[Fact]
+		public void GetLink() {
+			ContentServiceHelper helper = new ContentServiceHelper("en-us", "VS.90");
+			string link = helper.GetLink("T:System.String");
+			Assert.Contains("s1wwdcbf", link);
+		}
+
+		[Fact]
+		public void GetGenericLink() {
+			ContentServiceHelper helper = new ContentServiceHelper("en-us", "VS.90");
+			string link = helper.GetLink("M:System.Collections.Generic.IEnumerable`1.GetEnumerator");
+			Assert.Contains("s793z9y2", link);
+		}
+	}
+}
